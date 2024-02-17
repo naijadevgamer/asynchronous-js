01bd5c0be12edae0a3ca9ea0ce7961e189a2ff9f
thenaijagamer <sabdullahialaba50@gmail.com>
2024-02-17 20:57:43 +0100

    .

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+const request = new XMLHttpRequest();
+
+request.open('GET', 'https://restcountries.eu/rest/v2/name/nigeria');
+request.send();
+
+request.addEventListener('load', function () {
+  console.log(this.responseText);
+});
