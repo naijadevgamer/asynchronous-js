643c9b20a0eb61a4f9d967564453c4f4b5a2a718
thenaijagamer <sabdullahialaba50@gmail.com>
2024-02-23 00:10:07 +0100

    Keep it DRY

--- script.js
+++ script.js
@@ -68,24 +68,26 @@ const getCountryAndNeighbourOld = function (country) {
 
 // getCountryAndNeighbourOld('usa');
 
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(errorMsg);
+    return response.json();
+  });
+};
+
 // New way
 const getCountryAndNeighbourNew = function (country) {
   // Country 1
-  fetch('https://restcountries.com/v3.1/name/' + country)
-    .then(response => {
-      if (!response.ok) throw new Error('Country not found');
-      return response.json();
-    })
+  getJSON('https://restcountries.com/v3.1/name/' + country, 'Country not found')
     .then(([data]) => {
       renderCountry(data);
       if (!data.borders) throw new Error('Country is borderless');
       const [neighbour] = data.borders;
       // Country 2
-      return fetch('https://restcountries.com/v3.1/alpha/' + neighbour);
-    })
-    .then(response => {
-      if (!response.ok) throw new Error('Country not found');
-      return response.json();
+      return getJSON(
+        'https://restcountries.com/v3.1/alpha/' + neighbour,
+        'Country not found'
+      );
     })
     .then(([data]) => renderCountry(data, 'neighbour'))
     .catch(err => {
